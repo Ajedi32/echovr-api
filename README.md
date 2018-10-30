767174e2a4a15124b0b9fe8353655e911ed06fcb
Ajedi32 <ajedi32.web@gmail.com>
2018-10-29 21:09:28 -0500

    Implement basic proof of concept

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Echo VR API
 
 Python bindings for Echo VR's HTTP API.
+
+## Getting started with development
+
+If you haven't already, [install Python 3](https://www.python.org/downloads/) and [Pipenv](https://pipenv.readthedocs.io/en/latest/install/).
+
+Now, to install all Python package dependencies, run:
+
+```
+pipenv install
+```
+
+## Usage
+
+For now:
+
+```
+pipenv run python ./echovr_api/__init__.py
+```
--- echovr_api/__init__.py
+++ echovr_api/__init__.py
@@ -1,1 +1,16 @@
-name = "echovr_api"
+import requests
+import json
+
+if __name__ == "__main__":
+    try:
+        response = requests.get('http://127.0.0.1/session')
+    except requests.exceptions.ConnectionError as e:
+        print({'error': "Connection refused. Make sure you're running Echo VR with the -http option and that you're in a match."})
+
+    response_text = response.text.rstrip('\0')
+
+    try:
+        response_object = json.loads(response_text)
+        print(response_object)
+    except json.decoder.JSONDecodeError as e:
+        print({'error': "Could not decode response. (Not valid JSON.)"})
