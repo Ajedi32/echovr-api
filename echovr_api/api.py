7a34860cec7ffc74ac140923829b1e55cb7a4644
Ajedi32 <ajedi32.web@gmail.com>
2018-10-30 20:17:22 -0500

    Create basic GameState object

--- echovr_api/api.py
+++ echovr_api/api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from game_state import GameState
 
 class API(object):
     """Instantiate an API interface for the given base_url."""
@@ -28,3 +29,21 @@ class API(object):
         response = requests.get(self._gamestate_url)
         response_text = response.text.rstrip('\0')
         return json.loads(response_text)
+
+    """
+    Return a GameState object representing the state of the current game session
+    as presented by the API.
+
+    Throws:
+
+    - `requests.exceptions.ConnectionError`
+        This exception will be thrown if the API is unavaible. This might
+        indicate that the user is not currently in a match, or that they didn't
+        launch Echo VR with the `-http` option.
+
+    - `json.decoder.JSONDecodeError`
+        This exception will be thrown if the data returned by the API is not
+        valid JSON. Likely indicates a bug in Echo VR or in this library.
+    """
+    def fetch_state(self):
+        return GameState(**self.fetch_state_data())
