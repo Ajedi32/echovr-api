2c296043bea504dffae077558559a45558eace69
Ajedi32 <ajedi32.web@gmail.com>
2018-10-30 19:05:57 -0500

    Make CLI output pretty-printed JSON

--- echovr_api/__init__.py
+++ echovr_api/__init__.py
@@ -1,14 +1,17 @@
 import api
 from requests.exceptions import ConnectionError
-from json.decoder import JSONDecodeError
+import json
+
+def _output_json(data):
+    print(json.dumps(data, indent=2))
 
 if __name__ == "__main__":
     api = api.API()
     try:
         response_object = api.fetch_state_data()
     except ConnectionError as e:
-        print({'error': "Connection refused. Make sure you're running Echo VR with the -http option and that you're in a match."})
-    except JSONDecodeError as e:
-        print({'error': "Could not decode response. (Not valid JSON.)"})
+        _output_json({'error': "Connection refused. Make sure you're running Echo VR with the -http option and that you're in a match."})
+    except json.decoder.JSONDecodeError as e:
+        _output_json({'error': "Could not decode response. (Not valid JSON.)"})
 
-    print(response_object)
+    _output_json(response_object)
