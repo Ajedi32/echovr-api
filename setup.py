d31933ff48e6de3f710a39f375f8b56c5092ea02
mateuszdrwal <drwal.mateusz@gmail.com>
2018-11-07 16:55:53 +0100

    change example url to actual repository url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python bindings for Echo VR's HTTP API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Ajedi32/echovr-api/",
     packages=setuptools.find_packages(),
     install_requires=[
         "requests"
